8922d9707cecf666c517a64150de0d41e396fc0e
Ilton Pfleger Júnioe <pfleger@lisha.ufsc.br>
2026-07-31 11:05:09 -0300

    sync

--- payload/PhilosophersDinner/Traits.hpp
+++ payload/PhilosophersDinner/Traits.hpp
@@ -5,8 +5,7 @@
 namespace QUARK {
 
 template <> struct Traits<Payload> {
-    static constexpr size_t Size         = 256 * 1024;
-    static constexpr uintptr_t Address   = Traits<MemoryMap>::RamStart + Size * 2;
+    static constexpr uintptr_t Address   = Traits<MemoryMap>::RamStart + 1 * 1024 * 1024;
     static constexpr bool Virtualization = false;
     static constexpr bool Unprivileged   = false;
 };
