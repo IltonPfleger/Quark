5bb8c710401c7fa4af245ec10d318fd8a45e4b90
Ilton Pfleger Júnioe <pfleger@lisha.ufsc.br>
2026-09-15 10:16:08 -0300

    small optimizations

--- architecture/riscv64/include/sbi/LoadAccessFault.hpp
+++ architecture/riscv64/include/sbi/LoadAccessFault.hpp
@@ -16,30 +16,29 @@ class LoadAccessFault {
 public:
   static constexpr uint32_t CODE = 5;
 
-  static void dispatch(ContextFrame *c) {
-    if ((c->status & MachineMode::PP) == MachineMode::PP_M)
-      ExceptionHandler::esr(c);
+  static void dispatch(ContextFrame *context) {
+    if ((context->status & MachineMode::PP) == MachineMode::PP_M) [[unlikely]]
+      ExceptionHandler::esr(context);
 
     uintptr_t address = PageTable::virt2phys(csrr<MachineMode::TVAL>());
-    uintptr_t pc = PageTable::virt2phys(c->pc);
+    uintptr_t pc = PageTable::virt2phys(context->pc);
     uint16_t compressed = Decoder::compressed(pc);
     uint8_t i;
+
     if (compressed) {
       i = Decoder::rd(compressed);
+      context->pc += 2;
     } else {
       uint32_t instruction = Decoder::uncompressed(pc);
       i = Decoder::rd(instruction);
+      context->pc += 4;
     }
 
-    if (VirtualCPU::read(address, reinterpret_cast<uint32_t *>(&(*c)[i]))) {
-      if (compressed)
-        c->pc += 2;
-      else
-        c->pc += 4;
-      return;
-    };
+    uint32_t response = 0;
+    if (!VirtualCPU::read(address, &response)) [[unlikely]]
+      ExceptionHandler::esr(context);
 
-    ExceptionHandler::esr(c);
+    (*context)[i] = response;
   };
 };
 
